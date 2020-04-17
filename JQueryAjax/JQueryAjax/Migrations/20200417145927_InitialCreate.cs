f171aa7507bfec1fbd5a3a478890548c71f2b719
Rigoberto Estrella Sosa <estrella_rigoberto@hotmail.com>
2020-04-17 11:04:06 -0400

    Crea modelo, crea base de datos

---
+++ JQueryAjax/JQueryAjax/Migrations/20200417145927_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JQueryAjax.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "transactionModels",
+                columns: table => new
+                {
+                    TransactionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountNumber = table.Column<string>(type: "nvarchar(12)", nullable: true),
+                    BeneficiaryName = table.Column<string>(type: "nvarchar(12)", nullable: true),
+                    BankName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    SWIFTCode = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    Amount = table.Column<string>(type: "nvarchar(11)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_transactionModels", x => x.TransactionId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "transactionModels");
+        }
+    }
+}
---
+++ JQueryAjax/JQueryAjax/Models/TransactionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JQueryAjax.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TransactionModel> transactionModels { get; set; }
+    }
+}
